3c82e71e605a9c63d51d200dbba813a59b9ad162
1up-dev <121074287+1up-dev@users.noreply.github.com>
2023-04-18 21:21:01 -0400

    improve readability of getRacers()

--- src/gsheets.py
+++ src/gsheets.py
@@ -3,44 +3,30 @@ import requests
 import settings
 
 def getRacers():
-    success=False
-    tries = 0
     sheetRacers = []
     race_num = 0
     if 'race-num' in settings.modeInfo:
         race_num = settings.modeInfo['race-num']
 
     url = settings.gsheet + settings.google_api_key
+    response = requests.get(url, headers={})
+    if response.status_code not in range(200,300):
+        response = json.loads(response.content.decode("UTF-8"))["error"]
+        print('[!] Google Sheets API request failed. ' + str(response["code"]) +': '+response["message"])
+        return []
 
-    while not success:
-        response = requests.get(url, headers={})
-        tries+=1
-        if response.status_code in range(200,300):
-            nResponse = json.loads(response.content.decode("UTF-8"))["values"]
-            for e in nResponse:
-                if e == []:
-                    continue
-                elif e[0] == "Theoretical WR":
-                    continue
-
-                if race_num <= 0:
-                    sheetRacers.append(e[0].strip())
-                else:
-                    if len(e) < race_num+1:
-                        # not racing
-                        pass
-                    elif(e[race_num] != ''):
-                        # racing
-                        sheetRacers.append(e[0].strip())
-                    else:
-                        # not racing
-                        pass
-                
-            success=True
-        else:
-            nResponse = json.loads(response.content.decode("UTF-8"))["error"]
-            print('[!] (Try '+str(tries)+') Google Sheets API request failed. ' + str(nResponse["code"]) +': '+nResponse["message"])
-            if tries==5:
-                print("[!] Can't request from Google Sheets API. Giving up.")
-                success=True
+    response = json.loads(response.content.decode("UTF-8"))
+    for row in response["values"]:
+        if row == [] or row[0] == "Theoretical WR":
+            break
+        if race_num <= 0:
+            sheetRacers.append(row[0].strip())
+            continue
+        if len(row) < race_num+1:
+            # not racing
+            continue
+        if(row[race_num] != ''):
+            # racing
+            sheetRacers.append(row[0].strip())
+    
     return sheetRacers
